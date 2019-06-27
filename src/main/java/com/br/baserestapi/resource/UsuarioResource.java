99f564ef9a1724c4284525782d316e2d45d5262e
Gullit Menezes <gullit.menezes@gmail.com>
2019-06-27 15:54:31 -0300

    Ajustes para o funcionamento integrado com a interface

--- src/main/java/com/br/baserestapi/resource/UsuarioResource.java
+++ src/main/java/com/br/baserestapi/resource/UsuarioResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.baserestapi.model.Usuario;
@@ -21,7 +22,7 @@ public class UsuarioResource {
 	
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
-	public ResponseEntity<List<Usuario>> pesquisar() {
-		return ResponseEntity.ok(repository.findAll());
+	public ResponseEntity<List<Usuario>> pesquisar(@RequestParam(required = false, defaultValue = "") String nome) {
+		return ResponseEntity.ok(repository.findByNomeContaining(nome));
 	}
 }
